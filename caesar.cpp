07f08f53e7b447b224604c4e6bab825ed31bfea2
UnleashTheCode <danteodorandrei@outlook.com>
2020-03-15 16:48:56 +0200

    Decryption func

--- caesar.cpp
+++ caesar.cpp
@@ -6,18 +6,40 @@ int main(){
     string c;
     char n[50];
     int k;
-    cout << "Enter a message to be encrypted: ";
-    getline(cin, c);
-    cout << "Enter Key: ";
-    cin >> k;
-    for (int i; i < c.size();i++){
-        if (c[i] != ' '){
-        n[i] = char(int(c[i]) + k);
-        }
-        else{
-            n[i] += ' ';
-        }
+    char mod;
+    cout << "What do you want to do enc(e)/dec(d): ";
+    cin >> mod;
+    if (mod == 'e'){
+        cout << "Enter a message to be encrypted: ";
+        cin.ignore();
+        getline(cin, c);
+        cout << "Enter Key: ";
+        cin >> k;
+        for (int i=0; i < c.size();i++){
+            if (c[i] != ' '){
+                n[i] = char(int(c[i]) + k);
+            }
+            else{
+                n[i] += ' ';
+            }
     }
-    cout << "Encrypted message: " << n;
+    cout << "Encrypted message: " << n << endl;
+    }
+    else{
+        cout << "Enter a message to be decrypted: ";
+        cin.ignore();
+        getline(cin, c);
+        cout << "Enter Key: ";
+        cin >> k;
+        for (int i = 0; i < c.size();i++){
+            if (c[i] != ' '){
+                n[i] = char(int(c[i]) - k);
+            }
+            else{
+                n[i] += ' ';
+            }
+        }
+        cout << "Decrypted message: " << n << endl;
+    }
     return 0;
 }
